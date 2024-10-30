9407207ff17577d2c1607ec4146e9e6f36a191af
Gillean999 <202408704682@alunos.estacio.br>
2024-10-30 00:58:11 -0300

    delete.php

--- delete.php
+++ delete.php
@@ -1,7 +1,10 @@
-</php
+<?php
 include 'db.php';
 $id = $_get['id"];
 
 if ($conn->query($sql) === true) {
-
+    header("location: index.php");
+}else {
+   echo "erro ao exluir tarefa:" . $conn->error;
+}
 ?>
